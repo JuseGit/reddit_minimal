366bc09adde0c091b845109fa4738ec15eacfb67
juse <jusegit@gmail.com>
2021-06-16 16:30:00 +0200

    Complete vote counter with style setting. Other style settings.

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux'
 import * as styles from './post.module.css';
+import * as shared from './sharedStyles.module.css';
 import { StaticImage } from 'gatsby-plugin-image';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import CommentsList from './commentsList.js';
@@ -14,6 +15,7 @@ const Post = ({ id, name, subreddit, author, topic, time_frame, n_comments, img_
 	const [showCommentBox, setShowCommentBox] = useState(false);
 	let commentBoxStyle = showCommentBox ? {display: "block", visibility: "visible"} : {display: "none", visibility: "hidden"};
 	const posted_time = getPostedTime(time_frame);
+	const comment_btn_style = showCommentBox ? { color: "blue" } : { color: "initial" };
 
 
 	useEffect( () => {
@@ -35,23 +37,24 @@ const Post = ({ id, name, subreddit, author, topic, time_frame, n_comments, img_
 				<div className={styles.postFooter}>
 					<div style={{height:"100%", alignSelf:"center", border:"2px solid green"}}> {author} </div>
 					<div style={{alignSelf:"center", border:"2px solid green"}}> {posted_time} </div>
-					<div className={styles.commentBoxBtn} data-testid='show-comments-btn' onClick={() => setShowCommentBox(!showCommentBox)}>
-						<StaticImage
-						  src="../images/gatsby-icon.png"
-						  width={25}
-						  quality={95}
-						  formats={["AUTO", "WEBP", "AVIF"]}
-						  alt="A Gatsby astronaut"
-						  style={{ borderRadius: "25px", border:"2px solid green", marginRight: "10px", height:"25px", alignSelf:"center"}}
-						/>
+					<div className={styles.commentBtnContainer} data-testid='show-comments-btn' onClick={() => setShowCommentBox(!showCommentBox)}>
+						<button className={shared.icon_button.concat(' ', styles.commentBoxBtn)} style={comment_btn_style}>
+							<svg className={shared.icon_svg} version="1.2" baseProfile="tiny" viewBox="0 0 22 22">
+								<path d="M18 7c.542 0 1 .458 1 1v7c0 .542-.458 1-1 1h-8.829l-.171.171v-.171h-3c-.542 0-1-.458-1-1v-7c0-.542.458-1 1-1h12m0-2h-12c-1.65 0-3 1.35-3 3v7c0 1.65 1.35 3 3 3h1v3l3-3h8c1.65 0 3-1.35 3-3v-7c0-1.65-1.35-3-3-3z"></path>
+							</svg>
+						</button>
 						<p style={{alignSelf:"center", border:"2px solid green"}}> {n_comments} </p>
 					</div>
 				</div>
+
+				<div className={styles.commentBox} style={commentBoxStyle} aria-label="post-comments">
+					<CommentsList postName={name}/>
+				</div>
 			</article>
 
-			<article className={styles.commentBox} style={commentBoxStyle} aria-label="post-comments">
+			{/* <article className={styles.commentBox} style={commentBoxStyle} aria-label="post-comments">
 				<CommentsList postName={name}/>
-			</article>
+			</article> */}
 		</div>
 	);
 }
