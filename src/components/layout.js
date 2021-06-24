277599654449047eec919ce8e7ffa2a103f0e074
juse <jusegit@gmail.com>
2021-06-24 15:11:01 +0200

    Revision of index page and layout component.

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,32 +5,55 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import "./layout.css";
+import * as styles from "./layout_custom.module.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
- // <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-  return (
-    <>
-      <div>
-        <main>{children}</main>
-      </div>
-    </>
-  )
-}
+
+
+/**
+ * Layout - Component that arranges a page elements.
+ *
+ * @param  children	SEO component.
+ * @param  header	Component/DOM element to be rendered in the header section.
+ * @param  main     Component/DOM element to be rendered in the main section.
+ * @param  aside 	Component/DOM element to be rendered in the aside section.
+ * @return 			The newly arranged page.
+ */
+const Layout = ({ children, header, main, aside }) => {
+	const data = useStaticQuery(graphql`
+		query SiteTitleQuery {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+		`)
+		// <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+		return (
+			<>
+			{children}
+			<div className={styles.container}>
+				<header className={styles.cellItem}>
+					{header}
+				</header>
+
+				<main className={styles.cellItem}>
+					{main}
+				</main>
+
+				<aside className={styles.cellItem}>
+					{aside}
+				</aside>
+			</div>
+			</>
+		)
+	}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
